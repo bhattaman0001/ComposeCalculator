ab064c481db6de4da71c143961eea0a03964db45
Aman Bhatt <Bhattaman0001@gmail.com>
2024-02-16 19:25:02 +0530

    1. Calculator Application with Jetpack Compose 2. Custom Icon

--- app/src/main/java/com/example/calculator/ui/theme/Color.kt
+++ app/src/main/java/com/example/calculator/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val MediumGray = Color(0xFF686869)
+val LightGray = Color(0xFF929196)
+val Orange = Color(0xFFFF5100)
